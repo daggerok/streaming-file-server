1f657dadbdfd56738cd973c8ad8f8e98283329d3
Maksim Kostromin <daggerok@gmail.com>
2016-04-06 04:58:53 +0300

    add facebook connect

--- src/main/java/com/daggerok/spring/streaming/fileserver/config/web/WebInterceptor.java
+++ src/main/java/com/daggerok/spring/streaming/fileserver/config/web/WebInterceptor.java
@@ -1,7 +1,14 @@
 package com.daggerok.spring.streaming.fileserver.config.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.social.connect.Connection;
+import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.facebook.api.Facebook;
 import org.springframework.stereotype.Component;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.context.request.WebRequest;
@@ -11,22 +18,33 @@ import javax.annotation.PostConstruct;
 import javax.servlet.ServletContext;
 import java.util.Optional;
 
+@Slf4j
 @Component
 public class WebInterceptor implements WebRequestInterceptor {
 
     private static final ModelMap modelMap = new ModelMap();
+
     @Value("${app.upload.url}")
     String uploadUrl;
+
     @Value("${app.download.url}")
     String downloadUrl;
+
     @Value("${app.github.url}")
     String githubUrl;
+
     @Autowired
     ServletContext servletContext;
 
+    @Autowired
+    ConnectionRepository connectionRepository;
+
+    @Autowired
+    Facebook facebook;
+
     @PostConstruct
     public void setUp() {
-        final String ctx = servletContext.getContextPath();
+        val ctx = servletContext.getContextPath();
 
         modelMap.put("ctx", ctx);
         modelMap.put("webjars", ctx.concat("/webjars"));
@@ -41,7 +59,24 @@ public class WebInterceptor implements WebRequestInterceptor {
 
     @Override
     public void postHandle(WebRequest request, ModelMap model) throws Exception {
-        Optional.ofNullable(model).ifPresent(m -> m.putAll(modelMap));
+        Optional.ofNullable(model).ifPresent(m -> {
+            m.putAll(modelMap);
+
+            Optional.ofNullable(connectionRepository).ifPresent(cr ->
+                Optional.ofNullable(facebook).ifPresent(f ->
+                    Optional.ofNullable(cr.findPrimaryConnection(Facebook.class))
+                            .ifPresent(user -> {
+                                logJson(user);
+                                m.put("user", user);
+                            })));
+        });
+    }
+
+    @SneakyThrows
+    private void logJson(Object obj) {
+        if (null == obj) return;
+
+        log.info("{}\n{}", obj.getClass().getSimpleName(), new ObjectMapper().writeValueAsString(obj));
     }
 
     @Override
