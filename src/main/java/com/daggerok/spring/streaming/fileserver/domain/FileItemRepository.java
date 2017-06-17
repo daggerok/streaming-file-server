7666b3159c9883cc13bda626b0a25ece609adbbb
Maksim Kostromin <daggerok@gmail.com>
2017-06-17 19:31:46 +0300

    add querydsl repo; update footer for the years.

--- src/main/java/com/daggerok/spring/streaming/fileserver/domain/FileItemRepository.java
+++ src/main/java/com/daggerok/spring/streaming/fileserver/domain/FileItemRepository.java
@@ -1,6 +1,7 @@
 package com.daggerok.spring.streaming.fileserver.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +9,7 @@ import java.util.Optional;
 import java.util.stream.Stream;
 
 @Repository
-public interface FileItemRepository extends JpaRepository<FileItem, Long> {
+public interface FileItemRepository extends JpaRepository<FileItem, Long>, QueryDslPredicateExecutor<FileItem> {
 
     Optional<FileItem> findById(@Param("id") long id);
 
