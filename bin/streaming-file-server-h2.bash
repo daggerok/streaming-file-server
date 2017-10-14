72d35f50103892a0180d1aa86d97fdd715213372
Maksim Kostromin <daggerok@gmail.com>
2017-10-14 08:41:50 +0300

    fix streaming-file-server-h2.bash

--- bin/streaming-file-server-h2.bash
+++ bin/streaming-file-server-h2.bash
@@ -8,10 +8,12 @@ LOG_LEVEL=info
 # app info
 VERSION=2.2.0
 FILENAME="streaming-file-server-${VERSION}.jar"
+ITEMS_SERVICE_FILENAME="file-items-rest-service-${VERSION}.jar"
 
 if [ "'$LOG_LEVEL'" == "'debug'" ]; then
   echo "version                   : $VERSION"
-  echo "application filename      : $FILENAME"
+  echo "web application           : $FILENAME"
+  echo "data-layer application    : $ITEMS_SERVICE_FILENAME"
   echo
 fi
 
@@ -86,14 +88,17 @@ function GET_APPLICATION_FUNC {
     fi
 
     ${WGET} "https://github.com/daggerok/streaming-file-server/releases/download/$VERSION/$FILENAME"
+    ${WGET} "https://github.com/daggerok/streaming-file-server/releases/download/$VERSION/$ITEMS_SERVICE_FILENAME"
   fi
 }
 
 function START_APPLICATION_FUNC {
   GET_APPLICATION_FUNC
 
+  ${BASH_CMD} ${ITEMS_SERVICE_FILENAME} --spring.profiles.active=db-h2
+
   ${MKDIR} -p "$FILE_STORAGE_PATH"
-  ${BASH_CMD} ${FILENAME} --spring.profiles.active=db-h2 --app.upload.path="$FILE_STORAGE_PATH"
+  ${BASH_CMD} ${FILENAME} --app.upload.path="$FILE_STORAGE_PATH"
 }
 
 if [ "'$APPLICATION_COMMAND'" == "'start'" ]; then
@@ -101,17 +106,19 @@ if [ "'$APPLICATION_COMMAND'" == "'start'" ]; then
 fi
 
 function STOP_APPLICATION_FUNC {
-  APPLICATION_PID=$(${PS} waux|${GREP} ${FILENAME}|${GREP} -v 'grep'|${AWK} '{print $2}')
+  for F_NAME in ${FILENAME} ${ITEMS_SERVICE_FILENAME}; do
+    APPLICATION_PID=$(${PS} waux|${GREP} ${F_NAME}|${GREP} -v 'grep'|${AWK} '{print $2}')
 
-  if [ "'$LOG_LEVEL'" == "'info'" ]; then
-    echo "killing application by pid '$APPLICATION_PID' if exists"
-  fi
-
-  for P_ID in "$APPLICATION_PID"; do
-    if [ "''" != "'$P_ID'" ]; then
-      echo "${KILL} -9 $P_ID";
-      ${KILL} "-9" ${P_ID}
+    if [ "'$LOG_LEVEL'" == "'info'" ]; then
+      echo "killing application by pid '$APPLICATION_PID' if exists"
     fi
+
+    for P_ID in "$APPLICATION_PID"; do
+      if [ "''" != "'$P_ID'" ]; then
+        echo "${KILL} -9 $P_ID";
+        ${KILL} "-9" ${P_ID}
+      fi
+    done
   done
 }
 
@@ -124,11 +131,19 @@ function CLEANUP_FUNC {
   STOP_APPLICATION_FUNC
 
   if [ "'$LOG_LEVEL'" == "'info'" ]; then
-    echo "remove application file: '$FILENAME'"
+    echo "remove web app file: '$FILENAME'"
   fi
 
   if [ -f "$FILENAME" ]; then
     ${RM} -rf "$FILENAME"
+  fi
+
+  if [ "'$LOG_LEVEL'" == "'info'" ]; then
+    echo "remove data-layer app file: '$ITEMS_SERVICE_FILENAME'"
+  fi
+
+  if [ -f "$ITEMS_SERVICE_FILENAME" ]; then
+    ${RM} -rf "$ITEMS_SERVICE_FILENAME"
   fi
 
   if [ "'$FILE_STORAGE_PATH'" != "''" ]; then
